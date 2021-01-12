e54fb4838188d43e8e32a220b640905c3b67b20b
gabriel-avram-selab <gabrielavram1234@gmail.com>
2021-01-12 22:33:04 +0200

    Creare proiect cu javafx si maven

---
+++ src/main/java/ro/mta/Vremebuna/Main.java
@@ -0,0 +1,27 @@
+package ro.mta.Vremebuna;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URL;
+public class Main extends Application {
+
+
+    public static void main(String[] args)  {
+             launch(args);
+    }
+
+    public void start(Stage primaryStage) {
+        FXMLLoader loader = new FXMLLoader();
+
+        try {
+            loader.setLocation(this.getClass().getResource("/view/weather.fxml"));
+            primaryStage.setScene(new Scene(loader.load()));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
