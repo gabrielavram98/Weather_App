a84dbc4c0503b0130b3936e34bae674863839358
gabriel-avram-selab <gabrielavram1234@gmail.com>
2021-01-16 17:38:43 +0200

    Finalizare clasa Parser

--- src/main/java/ro/mta/se/lab/model/Parser.java
+++ src/main/java/ro/mta/se/lab/model/Parser.java
@@ -1,6 +1,45 @@
 package ro.mta.se.lab.model;
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonObject;
+
 
 public class Parser {
+     String raw;
+
+     public Parser(String raw) {
+          this.raw = raw;
+     }
+
+     public String getTemp(){
+          JsonObject items =Json.parse(raw).asObject().get("main").asObject();
+          return items.get("temp").toString();
+     }
+     public String getHumidity(){
+          JsonObject items =Json.parse(raw).asObject().get("main").asObject();
+          return items.get("humidity").toString();
+     }
+     public String getPrecipitation(){
+          JsonArray items =Json.parse(raw).asObject().get("weather").asArray();
+          JsonObject main= items.get(0).asObject();
+          return main.get("main").toString();
+
+
+     }
+     public String getWind(){
+          JsonObject items =Json.parse(raw).asObject().get("wind").asObject();
+          return items.get("speed").toString();
+     }
+
+
+
+
+
+
+
+
+
+
 
 
 }
